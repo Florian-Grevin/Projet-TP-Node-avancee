ba2c8f5c3837c7dc77d38d0f57911e7a206b9cd4
Florian-Grevin <grevflor@gmail.com>
2026-01-09 15:41:01 +0100

    Extraction de Données & DOM

--- test-scrap.js
+++ test-scrap.js
@@ -4,14 +4,23 @@ const puppeteer = require('puppeteer');
     // Lancement du navigateur (headless: false permet de le voir s'ouvrir)
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    const pageUrl = "http://books.toscrape.com/";
+
     // Navigation
-    await page.goto('http://books.toscrape.com/');
+    await page.goto(pageUrl);
 
     // Cette fonction s'exécute DANS la page web, pas dans votre terminal
-    const result = await page.evaluate(() => {
+    const result = await page.evaluate((pageUrl) => {
         // Code JavaScript standard (comme dans la console Chrome)
         const titre = document.title;
-        const produits = document.querySelectorAll('article.product_pod');   
+        const produits = document.querySelectorAll('article.product_pod');
+        
+
+        // for each produits -> produit
+        const titreLivre = document.querySelectorAll('h3 a');
+        const prixLivre = document.querySelectorAll('div .price_color');
+        const stockLivre = document.querySelectorAll('div p.instock.availability'); // y a <i> et le texte
+
         const produitsCnt = produits.length;     
         // Pour retourner des données vers Node.js
         return {
@@ -19,6 +28,26 @@ const puppeteer = require('puppeteer');
         nombreproduits: produitsCnt
         };
     });
-    console.log(result);
+
+    const data = await page.evaluate((pageUrl) => {
+            const results = [];
+            // On récupère TOUS les articles
+            const elements = document.querySelectorAll('article.product_pod');
+            elements.forEach(element => {
+                const titleRaw = element.querySelector('h3 a').getAttribute('title');
+                const priceRaw = element.querySelector('.price_color').innerText;
+                const stockRaw = element.querySelector('.instock.availability').innerText;
+                const imgRaw =  element.querySelector('.image_container img').getAttribute('src');
+                results.push({
+                    title: titleRaw,
+                    price: priceRaw.replace('£', ''),
+                    stock: stockRaw.trim(),
+                    url: pageUrl + imgRaw,
+                });
+            });
+            return results;
+    }, pageUrl);
+
+    console.log(data);
     await browser.close();
 })();
